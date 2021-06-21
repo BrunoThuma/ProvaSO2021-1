e9bc7673f6bed655f6dfa5d00366105736739e18
Bruno Thuma <40045005+BrunoThuma@users.noreply.github.com>
2021-06-21 19:48:55 -0300

    Finalmente funcionou

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ date_default_timezone_set ( 'America/Sao_Paulo' );
 echo "<html style='color: rgb(225, 225, 225); background-color: rgb(25, 25, 25);'>";
 echo "Prova 2 de SOCPS <br>";
 echo "Bruno Thuma <br>";
-echo "Data do carregamento da pagina".date("d/m/Y");
-echo "Hora do carregamento da pagina".date("H:i");
+echo "Data do carregamento da pagina ".date("d/m/Y");
+echo "Hora do carregamento da pagina ".date("H:i");
 echo "</html>";
 ?>
