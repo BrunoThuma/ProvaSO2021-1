caf7f419504ac369c759302a3dadff7d656a57f0
Bruno Thuma <40045005+BrunoThuma@users.noreply.github.com>
2021-06-21 19:47:46 -0300

    Brothers

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 date_default_timezone_set ( 'America/Sao_Paulo' );
 
-echo "<html style='color: rgb(225, 225, 225); background-color: rgb(25, 25, 25);'>"
-echo "<html style='Color: rgb(); Backgroundcolor'"
+echo "<html style='color: rgb(225, 225, 225); background-color: rgb(25, 25, 25);'>";
 echo "Prova 2 de SOCPS <br>";
 echo "Bruno Thuma <br>";
-echo "</html>"
+echo "Data do carregamento da pagina".date("d/m/Y");
+echo "Hora do carregamento da pagina".date("H:i");
+echo "</html>";
 ?>
