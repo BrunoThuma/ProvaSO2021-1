0430cc984634da29938c1016fac468137a126496
Bruno Thuma <40045005+BrunoThuma@users.noreply.github.com>
2021-06-21 19:36:56 -0300

    Added datetime

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-echo"Avaliação SOCPS II <br>";
-echo"Lucas Lourenço <br>";
-
-echo date();
+echo"Prova 2 de SOCPS <br>";
+echo"Bruno Thuma <br>";
+echo date("d/m/Y")
+echo date("h:i:sa");
 
 ?>
